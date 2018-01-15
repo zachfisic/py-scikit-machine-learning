1e7a94379781f76299488bc85448b0f7fa269a5a
Zach Fisic <zachfisic@gmail.com>
2018-01-15 10:55:42 -0800

    Perform regression in multiple_01

--- linear-regression/multiple_01.py
+++ linear-regression/multiple_01.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('./data/startups.csv')
 X = df.iloc[:, :-1].values
@@ -19,4 +20,8 @@ X = X[:, 1:]
 
 # Split dataset into training and test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Perform regression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
