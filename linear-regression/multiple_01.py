b8bad311e9355f1e0e3cc694663df2444d1d9290
Zach Fisic <zachfisic@gmail.com>
2018-01-15 11:20:20 -0800

    Add code for backward elimination strategy in multiple_01.py

--- linear-regression/multiple_01.py
+++ linear-regression/multiple_01.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.linear_model import LinearRegression
@@ -15,15 +16,30 @@ X[:, -1] = labelencoder_X.fit_transform(X[:, -1])
 onehotencoder = OneHotEncoder(categorical_features=[-1])
 X = onehotencoder.fit_transform(X).toarray()
 
-# Remove dummy categorical variable
+# Remove single dummy categorical variable
 X = X[:, 1:]
 
-# Split dataset into training and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# Add necessary constant of ones for stats model
+X = np.append(arr=np.ones(shape=(50,1), dtype=np.int8), values=X, axis=1)
 
-# Perform regression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+"""
+Multiple Linear Regression
 
-# Predict off test set
-y_pred = regressor.predict(X_test)
+Build model with optimal number of independent variables using Backward Elimination strategy
+"""
+sig_lvl = 0.05
+def backward_elimination(ivars, dvar, sl):
+  n = len(ivars[0])
+  for i in range(0, n):
+    # Run ordinary least squares algorithm
+    regressor_OLS = sm.OLS(endog=dvar, exog=ivars).fit()
+
+    # Remove independent variable with largest p-value (if larger than sig level)
+    max_pvalue = max(regressor_OLS.pvalues).astype(float)
+    if max_pvalue > sl:
+      for j in range(0, n - i):
+        if (regressor_OLS.pvalues[j].astype(float) == max_pvalue):
+          ivars = np.delete(ivars, j, 1)
+  return ivars
+
+X_modeled = backward_elimination(X, y, sig_lvl)
