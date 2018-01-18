478a4ec271ac307d611eb3f5f173a4412a896827
Zach Fisic <zachfisic@gmail.com>
2018-01-17 18:34:26 -0800

    Visualize results for polynomial example

--- polynomial-regression/polynomial_example_01.py
+++ polynomial-regression/polynomial_example_01.py
@@ -9,7 +9,17 @@ X = df.iloc[:, 1].values.reshape(-1,1)
 y = df.iloc[:, -1].values.reshape(-1,1)
 
 # Fit polynomial regression to data
-regressor = PolynomialFeatures(degree=2)
+regressor = PolynomialFeatures(degree=4)
 X_poly = regressor.fit_transform(X)
 l_reg = LinearRegression()
 l_reg.fit(X_poly, y)
+
+# Visualizing results
+X_grid = np.arange(min(X), max(X), step=0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color="red")
+plt.plot(X_grid, l_reg.predict(regressor.fit_transform(X_grid)), color="blue")
+plt.title('Polynomial Regression')
+plt.xlabel('position')
+plt.ylabel('salary')
+plt.show()
